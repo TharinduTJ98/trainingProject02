8005c24fe97391513ebe60a95c6106695c734a5d
Tharindu Jayawardhana <tharindujayawardha1@gmail.com>
2025-06-13 16:07:48 +0530

    set student data throgh student service class

--- src/app/Modules/registration/create-new-student/create-new-student.component.ts
+++ src/app/Modules/registration/create-new-student/create-new-student.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { StudentService } from '../Service/student.service';
+import { Student } from '../Modules/Student';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-new-student',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-new-student.component.scss']
 })
 export class CreateNewStudentComponent {
+  student: Student ={
+    id :0,
+    name: '',
+    email: '',
+    phone: '',
+  }
+  constructor(private studentService: StudentService, private router: Router){}
+
+  onsubmit(): void{
+    this.studentService.addStudent(this.student);
+    this.router.navigate(['/dashboard/reg/view-all-students']);
+    console.log("Student data", this.student)
+  }
 
 }
--- src/app/Modules/registration/show-all-students/show-all-students.component.ts
+++ src/app/Modules/registration/show-all-students/show-all-students.component.ts
@@ -1,23 +1,22 @@
-import { Component } from '@angular/core';
-interface Student {
-  id: number;
-  name: string;
-  email: string;
-  phone?: string;
-  course: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../Service/student.service';
+import { Student } from '../Modules/Student';
 @Component({
   selector: 'app-show-all-students',
   templateUrl: './show-all-students.component.html',
   styleUrls: ['./show-all-students.component.scss']
 })
-export class ShowAllStudentsComponent {
-  students: Student[] = [
-    {id:1, name:'ABC', email:'abc@gmail.com', phone:'077-6284276', course:'Computer Science'},
-    {id:2, name:'DEF', email:'def@gmail.com', phone:'077-4579852', course:'Mathematics'},
-    {id:3, name:'GHI', email:'ghi@gmail.com', phone:'077-4793584', course:'Science'},
-    {id:4, name:'JKL', email:'jkl@gmail.com', phone:'077-1498752', course:'Computer Science'},
-  ];
+export class ShowAllStudentsComponent implements OnInit {
+  students: Student[] = [];
+  constructor(private studentService: StudentService){
+  }
+  ngOnInit(): void {
+    this.studentService.students$.subscribe(data => {
+      this.students = data;
+    })
+  }
+
+
 
 
 }
